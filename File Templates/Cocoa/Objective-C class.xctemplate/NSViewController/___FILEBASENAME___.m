806af0da36c46e5b423228905ac9e6c67844b776
Korich <korich-vip@yandex.ru>
2014-08-14 12:28:32 +0300

    Added templates

---
+++ File Templates/Cocoa/Objective-C class.xctemplate/NSViewController/___FILEBASENAME___.m
@@ -0,0 +1,77 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Imports
+
+#import "___FILEBASENAME___.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Constants
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Private interface
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark -
+#pragma mark Properties
+
+#pragma mark -
+#pragma mark Initialization & Release
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    // Do any additional setup after loading the view from its nib.
+    
+    [super viewDidLoad];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    // Called when the view has been fully transitioned onto the screen. Default does nothing
+    
+    [super viewDidAppear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    // Called after the view was dismissed, covered or otherwise hidden. Default does nothing
+    
+    [super viewDidDisappear:animated];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Dispose of any resources that can be recreated.
+    
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark -
+#pragma mark Public methods
+
+#pragma mark -
+#pragma mark Private methods
+
+@end
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ XcodeTemplates
 ==============
 
 Templates for Xcode 5.x
+
+--------------
+
+Put "File Templates" folder to ~/Library/Developer/Xcode/Templates
